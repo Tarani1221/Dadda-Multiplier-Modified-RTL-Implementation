d076ccc3ee2fb08c1853d12169a1499664fee2e1
Tarani1221 <115485209+Tarani1221@users.noreply.github.com>
2024-04-18 17:50:34 +0530

    FullDadda_4bit_using_HA_CSLA_BEC1_testbench.sv

---
+++ FullDadda_4bit_using_HA_CSLA_BEC1_testbench.sv
@@ -0,0 +1,18 @@
+module tb();
+  wire [7:0]s;
+  reg [3:0]a,b;
+  
+  dadda_multiplier d1(s,a,b);
+  initial
+    begin
+      $dumpfile("dump.vcd");
+      $dumpvars(1);
+      
+      b = 4'b0000; // Corrected assignment
+      a = 4'b0000; // Corrected assignment
+
+      #10;
+      $display("%d",s[7:0]);
+      $finish;
+    end
+endmodule
